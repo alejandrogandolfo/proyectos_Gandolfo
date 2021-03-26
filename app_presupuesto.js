ca4fca90558694d25dd9a418ce325a1229962490
alejandrogandolfo <62605800+alejandrogandolfo@users.noreply.github.com>
2021-03-26 20:00:17 -0300

    Update app_presupuesto.js

--- app_presupuesto.js
+++ app_presupuesto.js
@@ -241,47 +241,6 @@ eye.addEventListener("click", function(){
 
 
 
-$(document).ready(function(){
-
-
-
-
-	
-	$('.categ').click(function(){
-		var catEgreso = $(this).attr('value');
-		console.log(catEgreso);
-
-
-
-		$('.egreso_descripcion1').hide();
-		$('.valor_egreso1').hide();
-		$('.cate').hide();
-		$('.close1').hide();
-
-		$('.egreso_descripcion1[value="'+catEgreso+'"]').show();
-		$('.valor_egreso1[value="'+catEgreso+'"]').show();
-		$('.cate[value="'+catEgreso+'"]').show();
-		$('.close1[value="'+catEgreso+'"]').show();
-		
-
-
-		
-	});
-
-
-	$('.categ[value="todo"]').click(function(){
-		$('.egreso_descripcion1').show();
-		$('.valor_egreso1').show();
-		$('.cate').show();
-		$('.close1').show();
-
-
-	});
-
-
-
-
-});
 
 
 
